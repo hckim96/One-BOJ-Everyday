0498f6b26d529ac33995f3170e596319bf7119f5
hckim96 <hyeoncheolkim96@gmail.com>
2020-11-07 13:06:50 +0900

    5582-DP

---
+++ G5-5582/5582.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+string str1, str2;
+
+int dp[4001][4001] = {0};
+int answer = 0;
+
+int main() {
+    cin >> str1 >> str2;
+
+    for (int i = 1; i <= str1.size(); i++) {
+        for (int j = 1; j <= str2.size(); j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                answer = max(answer, dp[i][j]);
+            }
+        }
+    }
+
+    cout << answer << "\n";
+}
