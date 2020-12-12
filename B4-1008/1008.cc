a404278d925dcccd4dba82ec93291115ed2ff355
hckim96 <hyeoncheolkim96@gmail.com>
2020-12-12 23:09:35 +0900

    1008 - cout precision

---
+++ B4-1008/1008.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double A, B;
+
+    cin >> A >> B;
+
+    cout.precision(9);
+    cout << fixed << A / B << "\n";
+}
---
+++ B4-1008/README.md
@@ -0,0 +1,4 @@
+# 1008 A/B
+
+- https://www.acmicpc.net/problem/1008
+- cout.precision(n) : if the floatfield format flag is set to fixed, display n digit after the decimal point
