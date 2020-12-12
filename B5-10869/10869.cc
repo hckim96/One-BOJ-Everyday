4cd87653992295d6b5db4c0a5cf064223ea950f5
hckim96 <hyeoncheolkim96@gmail.com>
2020-12-12 23:59:07 +0900

    10869

---
+++ B5-10869/10869.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
+}
---
+++ B5-10869/README.md
@@ -0,0 +1,3 @@
+# 10869 사칙연산
+
+- https://www.acmicpc.net/problem/10869
