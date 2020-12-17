9ef2b4d5e39567c255dd1a65bbde7d3997b86655
hckim96 <hyeoncheolkim96@gmail.com>
2020-12-18 07:36:33 +0900

    WIP

--- G4-16235/16235.cc
+++ G4-16235/16235.cc
@@ -9,7 +9,9 @@ using namespace std;
 int dr[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 int dc[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-pair<int, vector<int> > map[N_MAX + 1][N_MAX + 1];
+// age cnt
+vector<pair<int, int> > tree[N_MAX + 1][N_MAX + 1];
+int map[N_MAX + 1][N_MAX + 1];
 int A[N_MAX + 1][N_MAX + 1];
 
 int N, M, K;
@@ -22,32 +24,31 @@ int main() {
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
             cin >> A[i][j];
-            map[i][j].first = 5;
+            map[i][j] = 5;
         }
     }
 
     for (int i = 0; i < M; i++) {
         int x, y, z;
         cin >> x >> y >> z;
-        map[x][y].second.push_back(z);
+        tree[x][y].push_back({z, 1});
     }
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            sort(map[i][j].second.begin(), map[i][j].second.end(), less<int>());
-        }
-    }
     while (K--) {
-        vector<pair<int, int> > reproduce;
-        // 봄 여
+        int reproduce[N_MAX + 1][N_MAX + 1] = {0};
+
+        // 봄 여 번식 저장
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (!map[i][j].second.empty()) {
-                    for (auto it = map[i][j].second.rbegin(); it != map[i][j].second.rend(); it++) {
-                        if (map[i][j].first >= *it) {
-                            map[i][j].first -= *it;
-                            (*it)++;
-                            if (*it % 5 == 0) {
+                if (!tree[i][j].empty()) {
+                    for (auto it = tree[i][j].rbegin(); it != tree[i][j].rend(); it++) {
+                        auto [age, cnt] = *it;
+
+                        if (map[i][j] >= age * cnt) {
+                            map[i][j] -= age * cnt;
+                            it -> first++;
+
+                            if (it -> first % 5 == 0) {
                                 for (int dir = 0; dir < 8; dir++) {
                                     int nr, nc;
                                     nr = i + dr[dir];
@@ -56,15 +57,24 @@ int main() {
                                     if (nr < 1 || nr > N || nc < 1 || nc > N) {
                                         continue;
                                     }
-                                    reproduce.push_back({nr, nc});
+                                    reproduce[nr][nc] += it -> second;
                                 }
                             }
                         } else {
-                            while (it != map[i][j].second.rend()) {
-                                map[i][j].first += *it / 2;
-                                // map[i][j].second.erase(it++);
-                                map[i][j].second.erase((it.base() - 1));
+                            int canLive = map[i][j] / age;
+                            if (canLive > 0) {
+                                int die = cnt - canLive;
+
+                                map[i][j] -= age * canLive;
+                                map[i][j] += (age / 2) * die;
+                                it -> first++;
+                                it -> second = canLive;
                                 it++;
+                            }
+                            while (it != tree[i][j].rend()) {
+                                map[i][j] += (it -> first / 2) * it -> second;
+                                auto nit = decltype(it){tree[i][j].erase((++it).base())};
+                                it = nit;
                             }
                             break;
                         }
@@ -72,22 +82,23 @@ int main() {
                 }
             }
         }
+
+        // 번식, 겨
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                map[i][j].first += A[i][j];
+                tree[i][j].push_back({1, reproduce[i][j]});
+                map[i][j] += A[i][j];
             }
-        }
-        for (auto it = reproduce.begin(); it != reproduce.end(); it++) {
-            auto [x, y] = *it;
-            map[x][y].second.push_back(1);
         }
     }
 
     int ret = 0;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            if (!map[i][j].second.empty()) {
-                ret += map[i][j].second.size();
+            if (!tree[i][j].empty()) {
+                for (auto it = tree[i][j].begin(); it != tree[i][j].end(); it++) {
+                    ret += it -> second;
+                }
             }
         }
     }
