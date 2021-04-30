26aa30b4b552ee8ee2cb464e7b186ec8e2622d63
hckim96 <hyeoncheolkim96@gmail.com>
2021-04-30 16:13:26 +0900

    2741

---
+++ B3-2741/2741.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        cout << i << '\n';
+    }
+}
