75ed64ce7a4702f6e1c25c63f3aad16f926ee067
hckim96 <hyeoncheolkim96@gmail.com>
2021-09-26 15:56:31 +0900

    1084(*) - DP

---
+++ S1-10844/10844.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    const int MOD = 1000000000;
+
+    int N;
+    cin >> N;
+
+    vector<vector<int> > dp(N + 1, vector<int> (10, 0));
+
+    for (int i = 1; i <= 9; ++i) {
+        dp[1][i] = 1;
+    }
+
+    for (int i = 2; i <= N; ++i) {
+        dp[i][0] = dp[i - 1][1];
+        for (int j = 1; j < 9; ++j) {
+            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+        }
+        dp[i][9] = dp[i - 1][8];
+    }
+
+    long long answer = 0;
+    for (int i = 0; i < 10; ++i) {
+        answer += dp[N][i];
+    }
+    cout << answer % MOD << '\n';
+}
