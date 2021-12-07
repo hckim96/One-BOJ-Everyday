972c4d64894f4ea10ac8a1b99b9d28629973a36f
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-07 23:42:46 +0900

    1904 - DP

---
+++ S3-1904/1904.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+
+    vector<int> dp(N + 1, 0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= N; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] %= 15746;
+    }
+    cout << dp[N] << '\n';
+}
