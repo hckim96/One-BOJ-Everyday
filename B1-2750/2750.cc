7158b177c600f1fc30d4d84e3907004e05a56bfb
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-07 20:53:27 +0900

    2750

---
+++ B1-2750/2750.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+
+    vector<int> v(N);
+    for (auto& e: v) cin >> e;
+    sort(all(v));
+    for (auto e: v) cout << e << '\n';
+}
