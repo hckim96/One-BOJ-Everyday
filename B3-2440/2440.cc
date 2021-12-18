96b13ff394718a94802513da78426323e03a702e
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-19 03:01:39 +0900

    2440

---
+++ B3-2440/2440.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+
+    while (N--) {
+        for (int i = 0; i < N + 1; ++i) cout << '*';
+        cout << '\n';
+    }
+}
