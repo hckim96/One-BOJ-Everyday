a133c2d7c0329fbd1c0f26bd4ed725c197cd4b02
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-19 08:25:47 +0900

    7287

---
+++ B5-7287/7287.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    cout << 634 << '\n';
+    cout << "hckim96\n";
+}
