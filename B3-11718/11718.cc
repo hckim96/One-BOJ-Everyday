668eb0c000c4608bb04d0fb863a02dd6714c0b5f
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-23 01:13:52 +0900

    11718

---
+++ B3-11718/11718.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    string s;
+    
+    while (getline(cin, s)) {
+        cout << s << '\n';
+    }
+
+}
