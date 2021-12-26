98143c5526e946c2aafbfcdec8a482802af2c059
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-27 04:33:46 +0900

    1476

---
+++ S5-1476/1476.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    long long e, s, m;
+    cin >> e >> s >> m;
+
+    long long x = e;
+
+    while ((x - 1) % 28 + 1 != s || (x - 1) % 19 + 1 != m) {
+        x += 15;
+    }
+    cout << x << '\n';
+}
