5e7dff748f23f4f54fa1ccd9cd39fc6f3712486e
hckim96 <hyeoncheolkim96@gmail.com>
2021-12-28 23:52:16 +0900

    1094

---
+++ S5-1094/1094.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int X;
+    cin >> X;
+    cout << __builtin_popcount(X) << '\n';
+}
