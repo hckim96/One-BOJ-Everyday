83de75ce9582cd04ceeb1814307764afdd0485dc
hckim96 <hyeoncheolkim96@gmail.com>
2022-01-12 01:08:30 +0900

    3046

---
+++ B5-3046/3046.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    int R, S;
+    cin >> R >> S;
+    cout << 2 * S - R << '\n';
+}
