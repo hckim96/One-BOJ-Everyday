b3c7c9b2877599820a15c7bfca0ac781e2ab3991
hckim96 <hyeoncheolkim96@gmail.com>
2022-07-01 23:37:51 +0900

    2108 - Math

--- S4-2108/2108.cc
+++ S4-2108/2108.cc
@@ -1,45 +1,48 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
 
     int N;
     cin >> N;
 
-    vector<int> arr(N);
-    int sum = 0;
-    vector<int> cnt(8001, 0);
-
-    int maxCnt = 0;
-    for (auto & e:arr) {
+    vector<int> v(N);
+    map<int, int> m;
+    pair<int, int> many = {0, 0};
+    long long sum = 0;
+    for (auto& e: v) {
         cin >> e;
-        maxCnt = max(maxCnt, ++cnt[e + 4000]);
         sum += e;
+        ++m[e];
+        if (m[e] > many.second) {
+            many = {e, m[e]};
+        } else if (m[e] == many.second) {
+            if (e < many.first) {
+                many = {e, m[e]};
+            }
+        }
     }
-
-    int mode = 0;
-
-    for (int i = 0; i <= 8000; ++i) {
-        if (cnt[i] == maxCnt) {
-            if (mode) {
-                mode = i - 4000;
-                break;
-            } else {
-                mode = i - 4000;
+    // pair<int, int> ans = {-1, -1};
+    int ans = 1e9;
+    for (auto [n, cnt]: m) {
+        if (cnt == many.second && n != many.first) {
+            if (ans > n) {
+                ans = n;
             }
         }
     }
 
-    sort(arr.begin(), arr.end());
-
-    cout << round((double)sum / N) << '\n';
-    cout << arr[N / 2] << '\n';
-    cout << mode << '\n';
-    cout << arr[N - 1] - arr[0] << '\n';
+    sort(all(v));
+    cout << int(round((long double) sum / N)) << '\n';
+    cout << v[N / 2] << '\n';
+    cout << (ans != 1e9 ? ans : many.first) << '\n';
+    cout << v.back() - v.front() << '\n';
 }
