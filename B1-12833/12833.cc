65363819ab85c6ab0e037fffd21230799e037cdf
hckim96 <hyeoncheolkim96@gmail.com>
2022-07-24 17:55:16 +0900

    12833

---
+++ B1-12833/12833.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+
+  ll A, B, C;
+  cin >> A >> B >> C;
+  if (C & 1) cout << (A ^ B) << '\n';
+  else cout << A << '\n';
+}
