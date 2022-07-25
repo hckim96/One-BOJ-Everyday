9b24bc014f34ad9651ad647f826a872ec0a25b20
hckim96 <hyeoncheolkim96@gmail.com>
2022-07-25 21:14:26 +0900

    25083

---
+++ B5-25083/25083.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+
+  cout << "         ,r'\"7\n";
+  cout << "r`-_   ,'  ,/\n";
+  cout << " \\. \". L_r'\n";
+  cout << "   `~\\/\n";
+  cout << "      |\n";
+  cout << "      |\n";
+}
