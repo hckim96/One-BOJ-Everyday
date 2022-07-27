311a7407b7a7a0093a7f274d20f531ef38fbc95c
hckim96 <hyeoncheolkim96@gmail.com>
2022-07-28 00:44:34 +0900

    10699

---
+++ B5-10699/10699.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+
+  cout << "2022-07-28";
+}
