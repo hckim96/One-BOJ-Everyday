b07918f64622492dcc09f515934a0dcd6d380956
hckim96 <hyeoncheolkim96@gmail.com>
2022-11-16 18:31:28 +0900

    14581

---
+++ B5-14581/14581.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  string s;
+  cin >> s;
+  cout << ":fan::fan::fan:\n:fan::"<< s << "::fan:\n:fan::fan::fan:";
+}
