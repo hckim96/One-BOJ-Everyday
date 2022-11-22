33f310302c3bae26dd4874ce327dc4cb197ebbb0
hckim96 <hyeoncheolkim96@gmail.com>
2022-11-22 20:34:00 +0900

    15726

---
+++ B4-15726/15726.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll a, b, c;
+  cin >> a >> b >> c;
+
+  cout << max(int((long double)a * b / c),int( (long double) a / b * c)) << '\n';
+}
