34a1b1eb634eb515ca3bf841f5cada9292cbb484
hckim96 <hyeoncheolkim96@gmail.com>
2022-11-26 06:13:02 +0900

    2083

---
+++ B4-2083/2083.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  string n;
+  int a, w;
+  while (cin >> n >> a >> w && n != "#") {
+    cout << n << ' ';
+    if (a > 17 || w >= 80) {
+      cout << "Senior\n";
+    } else {
+      cout << "Junior\n";
+    }
+  }
+}
