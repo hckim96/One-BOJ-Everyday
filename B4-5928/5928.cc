5bd658c41cf63c80af44ae1763f4f08c6d3c1d51
hckim96 <hyeoncheolkim96@gmail.com>
2022-12-22 23:29:21 +0900

    5928

---
+++ B4-5928/5928.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll s = 11 * 60 * 24 + 11 * 60 + 11;
+  ll e = 0;
+  ll d, h, m;
+  cin >> d >> h >> m;
+  e = d * 60 * 24 + h * 60 + m;
+  if (e < s) {
+    cout << -1;
+  } else {
+    cout << e - s;
+  }
+}
