2cb51fd7ed2d3f82879b13f3a71d055f20f810e7
hckim96 <hyeoncheolkim96@gmail.com>
2022-12-24 09:37:11 +0900

    14425 - Set

---
+++ S3-14425/14425.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  int N, M;
+  cin >> N >> M;
+
+  set<string> st;
+  for (int i = 0; i < N; ++i) {
+    string s;
+    cin >> s;
+    st.insert(s);
+  }
+  ll ans = 0;
+  while (M--) {
+    string s;
+    cin >> s;
+    ans += st.count(s);
+  }
+  cout << ans << '\n';
+}
