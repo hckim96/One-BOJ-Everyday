e15c7eb96e2d5591fdd51a9e1947dfab4b992d8b
hckim96 <hyeoncheolkim96@gmail.com>
2023-01-17 10:05:56 +0900

    13300

---
+++ B2-13300/13300.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll n, k;
+  cin >> n >> k;
+  vl cnt1(7);
+  vl cnt2(7);
+  FOR (i, 0, n) {
+    int s, y;
+    cin >> s >> y;
+    if (s) {
+      //man
+      ++cnt1[y];
+    } else {
+      ++cnt2[y];
+
+    }
+  }
+  ll ans = 0;
+  FOR (i, 1, 7) {
+    ans += cnt1[i] / k + (cnt1[i] % k != 0);
+    ans += cnt2[i] / k + (cnt2[i] % k != 0);
+  }
+  cout << ans;
+}
