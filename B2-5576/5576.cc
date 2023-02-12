aaf9bab62cde26e32c467a6aaa0be02da4e8d27a
hckim96 <hyeoncheolkim96@gmail.com>
2023-02-12 20:29:29 +0900

    5576

---
+++ B2-5576/5576.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  vl v1(10);
+  vl v2(10);
+  for (auto& e: v1) cin >> e;
+  for (auto& e: v2) cin >> e;
+  sort(all(v1));
+  sort(all(v2));
+  cout << accumulate(v1.begin() + 7, v1.end(), 0L) << ' ';
+  cout << accumulate(v2.begin() + 7, v2.end(), 0L) << ' ';
+}
