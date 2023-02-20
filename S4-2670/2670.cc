1025d5d0354fc551237cfa4f285008d9d1eef0b1
hckim96 <hyeoncheolkim96@gmail.com>
2023-02-20 21:24:55 +0900

    2670(*) - DP

---
+++ S4-2670/2670.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  int n;
+  cin >> n;
+
+  ld m = 1.0;
+  ld pp = 1.0;
+  ld ans = 0.0;
+  for (int i = 1; i <= n; ++i) {
+    ld num;
+    cin >> num;
+    if (num == 0.0) {
+      m = 1.0;
+      pp = 1.0;
+      continue;
+    }
+    pp *= num;
+    ans = max(ans, pp / m);
+    m = min(m, pp);
+  }
+
+  cout.precision(3);
+  cout << fixed;
+  cout << ans << '\n';
+}
