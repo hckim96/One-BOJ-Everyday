9658136d0f4c61aa2c9368c920ff30fd6a01c94b
hckim96 <hyeoncheolkim96@gmail.com>
2023-02-20 22:01:45 +0900

    S1-2502 - DP

---
+++ S1-2502/2502.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll d, k;
+  cin >> d >> k;
+
+  vector<pair<int, int> > v(d + 1);
+  v[1] = {1, 0};
+  v[2] = {0, 1};
+  for (int i = 3; i <= d; ++i) {
+    v[i].first = v[i - 1].first + v[i - 2].first;
+    v[i].second = v[i - 1].second + v[i - 2].second;
+  }
+
+  for (int a = 1; a <= 100000; ++a) {
+
+    if ((k - a * v[d].first) % v[d].second == 0) {
+      cout << a << '\n';
+      cout << (k - a * v[d].first) / v[d].second << '\n';
+      break;
+    }
+  }
+}
