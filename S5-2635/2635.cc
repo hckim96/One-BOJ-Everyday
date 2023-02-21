4049f62b7f6bae0d67623aacf0843bdd7d1794e4
hckim96 <hyeoncheolkim96@gmail.com>
2023-02-22 00:20:18 +0900

    S5-2635 - Math

---
+++ S5-2635/2635.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll a;
+  cin >> a;
+  ll ans = 3;
+  ll b = a;
+  pair<ll, ll> prev = {1, 1};
+  pair<ll, ll> cur = {1, 2};
+  auto t = [&]() {
+    auto x1 = floor((ld) prev.first / prev.second * a);
+    auto x2=  floor((ld) cur.first / cur.second * a);
+    if (x1 != x2) {
+      b = max(x1, x2);
+      return false;
+    }else{
+      if (x1 == (ld) prev.first / prev.second * a || x2 == (ld) cur.first / cur.second * a) {
+        return false;
+      }
+      return true;
+    }
+
+  };
+
+  while (!t()) {
+    pair<ll, ll> next = {prev.first + cur.first, prev.second + cur.second};
+    prev = cur;
+    cur = next;
+    ++ans;
+  }
+
+  cout << ans << '\n';
+
+  ll pp = a;
+  ll p = b;
+  cout << pp << ' ';
+  cout << p << ' ';
+  for (ll i = 2; i < ans; ++i) {
+    ll c = pp - p;
+    cout << c << ' ';
+    pp = p;
+    p = c;
+  }
+}
