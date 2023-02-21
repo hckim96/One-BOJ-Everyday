0a552f32dcfce0030d47f21e5e9deaa035b4e5fa
hckim96 <hyeoncheolkim96@gmail.com>
2023-02-21 23:46:44 +0900

    B1-2526 - Implementation

---
+++ B1-2526/2526.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  vector<int> v;
+  int N, P;
+  cin >> N >> P;
+
+  v.push_back(N);
+  int tmp = 1000;
+  while (tmp--) {
+    auto next = v.back() * N % P;
+    for (int i = 0; i < v.size(); ++i) {
+      if (v[i] == next) {
+        cout << v.size() - i;
+        return 0;
+      }
+    }
+    v.push_back(next);
+  }
+}
