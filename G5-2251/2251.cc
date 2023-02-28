a2c3b2713cb81113dfe6cf3875be3428de64a69d
hckim96 <hyeoncheolkim96@gmail.com>
2023-02-28 22:24:30 +0900

    G5-2251 - BFS

--- G5-2251/2251.cc
+++ G5-2251/2251.cc
@@ -4,127 +4,64 @@ using namespace std;
 
 #define all(v) (v).begin(), (v).end()
 #define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<int>> vvi;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
   freopen("../input.txt", "r", stdin);
   freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
 
   int A, B, C;
-
   cin >> A >> B >> C;
-  vector<vector<vector<int> > > visited(201, vector<vector<int> > (201, vector<int> (201, 0)));
-
-  set<int> ans;
-
-  visited[0][0][C] = true;
   queue<tuple<int, int, int> > q;
   q.push({0, 0, C});
+  set<tuple<int, int, int> > vis;
+  set<int> ans;
+  vis.insert({0, 0, C});
   while (q.size()) {
     auto [a, b, c] = q.front();
-    if (a == 0)
-    ans.insert(c);
+    if (a == 0) ans.insert(c);
     q.pop();
-    int na, nb, nc;
+
     if (a) {
-      // a -> b
-      if (B - b >= a) {
-        nb = a + b;
-        na = 0;
-        nc = c;
-      } else {
-        na = a - B + b;
-        nb = B;
-        nc = c;
-      }
-      if (!visited[na][nb][nc]) {
-        visited[na][nb][nc] = true;
-        q.push({na, nb, nc});
-      }
-      // a -> c
-      if (C - c >= a) {
-        nc = a + c;
-        na = 0;
-        nb = b;
-      } else {
-        na = a - C + c;
-        nc = C;
-        nb = b;
-      }
-      if (!visited[na][nb][nc]) {
-        visited[na][nb][nc] = true;
-        q.push({na, nb, nc});
-      }
+      int tmp = min(B- b, a);
+      if (vis.insert({a - tmp, b + tmp, c}).second) q.push({a - tmp, b + tmp, c});
+      int tmp2 = min(C - c, a);
+      if (vis.insert({a - tmp2, b, c + tmp2}).second) q.push({a-tmp2, b, c+tmp2});
     }
-
     if (b) {
-      // b -> a
-      if (A - a >= b) {
-        na = b + a;
-        nb = 0;
-        nc = c;
-      } else {
-        nb = b - A + a;
-        na = A;
-        nc = c;
-      }
-      if (!visited[na][nb][nc]) {
-        visited[na][nb][nc] = true;
-        q.push({na, nb, nc});
-      }
-      // b -> c
-      if (C - c >= b) {
-        nc = b + c;
-        nb = 0;
-        na = a;
-      } else {
-        nb = b - C + c;
-        nc = C;
-        na = a;
-      }
-      if (!visited[na][nb][nc]) {
-        visited[na][nb][nc] = true;
-        q.push({na, nb, nc});
-      }
+      int tmp = min(A - a, b);
+      if (vis.insert({a + tmp, b - tmp, c}).second) q.push({a + tmp, b - tmp, c});
+      int tmp2 = min(C - c, b);
+      if (vis.insert({a, b - tmp2, c + tmp2}).second) q.push({a, b- tmp2, c + tmp2});
     }
-
     if (c) {
-      // c -> a
-      if (A - a >= c) {
-        na = a + c;
-        nc = 0;
-        nb = b;
-      } else {
-        nc = c - A + a;
-        na = A;
-        nb = b;
-      }
-      if (!visited[na][nb][nc]) {
-        visited[na][nb][nc] = true;
-        q.push({na, nb, nc});
-      }
-      // c -> b
-      if (B - b >= c) {
-        nb = b + c;
-        nc = 0;
-        na = a;
-      } else {
-        nc = c - B + b;
-        nb = B;
-        na = a;
-      }
-      if (!visited[na][nb][nc]) {
-        visited[na][nb][nc] = true;
-        q.push({na, nb, nc});
-      }
-
+      int tmp = min(A-a, c);
+      if (vis.insert({a + tmp, b, c - tmp}).second) q.push({a + tmp, b , c-tmp});
+      int tmp2 = min(B - b, c);
+      if (vis.insert({a, b + tmp2, c - tmp2}).second) q.push({a, b + tmp2, c - tmp2});
     }
-
   }
 
-  for (auto e: ans) {
-    cout << e << ' ';
-  }
-
+  each(e, ans) cout << e << ' ';
 }
