6c1848b135c9778946935be5a05a2136893d5e3c
hckim96 <hyeoncheolkim96@gmail.com>
2023-03-26 00:13:14 +0900

    B5-27433 - Math

---
+++ B5-27433/27433.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef vector<vector<int>> vvi;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll n;
+  cin >> n;
+  if (n == 0) {
+    cout << 1;
+    return 0;
+  }
+
+  ll ans = n;
+  while (--n > 0) {
+    ans *= n;
+  }
+  cout << ans;
+}
