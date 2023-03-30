c9f7597673110ce7b46c6bb1a3c1f7e412345d13
hckim96 <hyeoncheolkim96@gmail.com>
2023-03-30 10:15:45 +0900

    S5-14490 - GCD

---
+++ S5-14490/14490.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef vector<vector<int>> vvi;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll n, m;
+  scanf("%ld:%ld",&n, &m);
+  ll gcd_ = gcd(n, m);
+  n /= gcd_;
+  m /= gcd_;
+  printf("%ld:%ld",n, m);
+}
