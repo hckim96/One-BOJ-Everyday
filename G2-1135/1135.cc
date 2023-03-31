f914d4c6904a079dc6629a3c1ff1eb6e0c83f76b
hckim96 <hyeoncheolkim96@gmail.com>
2023-03-31 22:28:13 +0900

    G2-1135(**) - DP, Greedy, Sorting

---
+++ G2-1135/1135.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef vector<vector<int>> vvi;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  int n;
+  cin >> n;
+
+  vi p(n);
+  vi childCnt(n);
+  vector<vector<pair<int, int> > > adj(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> p[i];
+    if (p[i] != -1) {
+      adj[p[i]].push_back({0, i});
+    }
+  }
+
+  function<int(int)> f = [&](int cur) {
+    int ret = 0;
+
+    vi tmp;
+    for (int i = 0; i < adj[cur].size(); ++i) {
+      tmp.push_back(f(adj[cur][i].second));
+    }
+    sort(rall(tmp));
+
+    for (int i = 0; i < tmp.size(); ++i) {
+      ret = max(ret, i + 1 + tmp[i]);
+    }
+    
+    return ret;
+  };
+
+  cout << f(0);
+}
