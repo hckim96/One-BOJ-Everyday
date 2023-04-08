64f92cdd7f9d099a34f61b5ec430159312cec331
hckim96 <hyeoncheolkim96@gmail.com>
2023-04-08 21:41:45 +0900

    G4-12886 - BFS

---
+++ G4-12886/12886.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef vector<vector<int>> vvi;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> tlll;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  int a, b, c;
+  cin >> a >> b >> c;
+  vi v = {a, b, c};
+  sort(all(v));
+  set<vi> vis;
+  vis.insert(v);
+  queue<vi> q;
+  q.push(v);
+  while (q.size()) {
+    auto fv = q.front();
+    q.pop();
+
+    if (fv[0] == fv[1] && fv[1] == fv[2]) {
+      cout << 1;
+      return 0;
+    }
+
+    for (int i = 0; i < 3; ++i) {
+      for (int j = i + 1; j < 3; ++j) {
+        if (fv[i] == fv[j]) continue;
+        int k = 3 - i - j;
+        vi nv = {fv[k], fv[i] * 2, fv[j] - fv[i]};
+        sort(all(nv));
+        if (vis.count(nv)) continue;
+        vis.insert(nv);
+        q.push(nv);
+      }
+    }
+  }
+
+  cout << 0;
+}
