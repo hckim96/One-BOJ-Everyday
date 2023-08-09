8036d7b838b6fefd926802ba94b8fa0abc51bcfb
hckim96 <hyeoncheolkim96@gmail.com>
2023-08-09 15:12:25 +0900

    G2-11442 - Fibonacci sequence by exponentiation by squaring

---
+++ G2-11442/11442.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef vector<vector<int>> vvi;
+typedef pair<ll, ll> pl;
+typedef tuple<ll, ll, ll> tl;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+typedef vector<string> vs;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+ll MOD = 1e9 + 7;
+
+vvl operator*(vvl& v1, vvl& v2) {
+  vvl ret = {{0, 0}, {0, 0}};
+  for (int i = 0; i < v1.size(); ++i) {
+    for (int j = 0; j < v2[0].size(); ++j) {
+      for (int k = 0; k < v1[0].size(); ++k) {
+        ret[i][j] += v1[i][k] * v2[k][j];
+        ret[i][j] %= MOD;
+      }
+    }
+  }
+  return ret;
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  ll n;
+  cin >> n;
+
+  if (n & 1) ++n;
+  --n;
+  
+  vvl ret = {{1, 0}, {0, 1}};
+  vvl mul = {{1, 1}, {1, 0}};
+
+  while (n) {
+    if (n & 1) ret = ret * mul;
+    mul = mul * mul;
+    n >>= 1;
+  }
+
+  cout << ret[0][0];
+}
