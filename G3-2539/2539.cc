e31a23cb06da8b0eaa54e9326b87a7de8d424db7
hckim96 <hyeoncheolkim96@gmail.com>
2023-09-01 23:40:53 +0900

    G3-2539 - Parametric search

---
+++ G3-2539/2539.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define ll long long
+#define ld long double
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pair<int, int> > vpi;
+typedef vector<vector<int>> vvi;
+typedef pair<ll, ll> pl;
+typedef tuple<ll, ll, ll> tl;
+typedef vector<ll> vl;
+typedef vector<pair<ll, ll>> vpl;
+typedef vector<vector<ll>> vvl;
+typedef vector<string> vs;
+int dr[4] = {0, 0, 1, -1};
+int dc[4] = {1, -1, 0, 0};
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #if !defined(ONLINE_JUDGE)
+  freopen("../input.txt", "r", stdin);
+  freopen("../output.txt", "w", stdout);
+  #endif // ONLINE_JUDGE
+
+  int _;
+  cin >> _ >> _;
+  int num;
+  cin >> num;
+  int num2;
+  cin >> num2;
+
+  vi v(num2);
+  int l = 0;
+
+  for (int i = 0; i < num2; ++i) {
+    int r, c;
+    cin >> r >> c;
+    l = max(l, r);
+    v[i] = c;
+  }
+  l--;
+  sort(all(v));
+
+  auto check = [&](int x) {
+    auto it = v.begin();
+    int tmp = num;
+    while (tmp-- && it != v.end()) {
+      auto it2 = it;
+      while (it2 != v.end() && *it2 - *it + 1 <= x) {
+        ++it2;
+      }
+      it = it2;
+    }
+    return it == v.end();
+  };
+
+  int h = 1e7;
+
+  while (l + 1 < h) {
+    int mid = l + h >> 1;
+    if (check(mid)) h = mid;
+    else l = mid;
+  }
+  cout << h;
+}
